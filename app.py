3cafaa1793909c8e329fc1c7beed1f2a6c5b1ade
survi15 <satpathy.s@husky.neu.edu>
2018-10-02 11:53:24 -0700

    1. Created db model for Book & Borrower. 2. Connected to Heroku Postgres URI.

--- app.py
+++ app.py
@@ -1,6 +1,49 @@
+import datetime
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 app = Flask(__name__)
+app.config[
+    'SQLALCHEMY_DATABASE_URI'] = 'postgres://mtwahsiiuuodov:ea5fbd9f4720b2ddfbc9998cd490f8be902e86a01aa8b8b5e0fc31dc904f8219@ec2-50-17-225-140.compute-1.amazonaws.com:5432/dc86b8ec8qdppu'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class Book(db.Model):
+    id = db.Column('book_id', db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    author = db.Column(db.String(50))
+    genre = db.Column(db.String(200))
+    published_on = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+    notes = db.Column(db.String(50))
+    lent_to = db.Column(db.Integer)
+    to_be_returned_on = db.Column(db.DateTime)
+    is_returned = db.Column(db.Boolean)
+
+
+def __init__(self, title, author, genre, published_on, notes, lent_to, to_be_returned_on, is_returned):
+    self.title = title
+    self.author = author
+    self.genre = genre
+    self.published_on = published_on
+    self.notes = notes
+    self.lent_to = lent_to
+    self.to_be_returned_on = to_be_returned_on
+    self.is_returned = is_returned
+
+
+class Borrower(db.Model):
+    id = db.Column('borrower_id', db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+
+
+def __init__(self, name):
+    self.name = name
+
+
+db.create_all()
 
 
 @app.route('/')
