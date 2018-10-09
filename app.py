bf5896c56db2c4186c10e3f2f5928e6114dc9c4e
aditya2705 <rathi.ad@husky.neu.edu>
2018-10-08 21:55:39 -0700

    Added app context for db create all

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask import Flask, jsonify
 from book import book_app
 from borrower import borrower_app
 from config import database_uri
-from database.models import db, Borrower
+from database.models import db
 
 
 def create_app():
@@ -15,32 +15,18 @@ def create_app():
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     with app.app_context():
         db.init_app(app)
+        db.create_all()
     app.register_blueprint(book_app, url_prefix='/book')
     app.register_blueprint(borrower_app, url_prefix='/borrower')
 
     @app.route('/')
     def hello_world():
         return jsonify(response='Hello World! Heroku deployed'), HTTPStatus.OK
-
-    @app.route('/show')
-    def record_show():
-        test_record = Borrower.query.filter_by(name='Survi').first()
-        print(test_record.id, " ", test_record.name)
-        return test_record.name
-
-    @app.route('/delete')
-    def record_delete():
-        record = record_show()
-        before_delete = Borrower.query.filter_by(name=record).first()
-        db.session.delete(before_delete)
-        db.session.commit()
-        after_delete = Borrower.query.filter_by(name="Survi").first()
-        print(after_delete.id)
-        return "Successfully deleted"
     return app
 
 
+application = create_app()
+
+
 if __name__ == '__main__':
-    application = create_app()
-    db.create_all()
     application.run()
